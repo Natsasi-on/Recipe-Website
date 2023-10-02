72b4ede79815b39fe625ba89511b9b179c3c3ff8
Natsasion Srithiowat <100752017+Natsasi-on@users.noreply.github.com>
2023-10-02 14:21:37 -0400

    Update dish.php

--- dish.php
+++ dish.php
@@ -1,15 +1,26 @@
 <?php
+// Set error reporting to show all errors except notices
 error_reporting(E_ALL & ~E_NOTICE);
+
+// Include necessary configuration files and functions
 require_once 'includes/ini.php';
-include_once 'includes/functions.php';
+require_once 'includes/functions.php';
+
+// Start a session
 session_start();
+
+// Initialize variables
+$foodId = NULL;
+$commentError = $comment = $ratingError = $thestarRating = '';
+
+// Check if "foodid" is set in the URL
 if (!isset($_GET["foodid"])) {
     $foodId = NULL;
 } else {
     $foodId = $_GET["foodid"];
 }
 
-// after submit comment the url will restore to dish without foodid
+// After submitting a comment, the URL will be redirected to the dish page without "foodid"
 if ($foodId == NULL) {
     $foodId = $_SESSION['foodId'];
 } else {
@@ -17,27 +28,30 @@ if ($foodId == NULL) {
     $_SESSION['foodId'] = $foodId;
 }
 
+// Check if a user is logged in
 if (!isset($_SESSION["useridfromDB"])) {
     $userId = '';
 } else {
     $userId = $_SESSION["useridfromDB"];
 }
 
-$commentError = $comment = $ratingError = $thestarRating = '';
-
+// Handle comment submission
 if (isset($_POST['btnAddComment'])) {
-
+    // Check if the user is logged in
     if (!isset($_SESSION["userNameDB"])) {
         header("Location: login.php");
         exit();
     } else {
+        // Check if a star rating is selected
         if (($_POST["starRateDDl"]) == 0) {
             $ratingError = "Please select at least one";
         }
+        // Check if the comment is empty
         if (empty($_POST["commenttxt"])) {
             $commentError = 'Cannot be blank!';
         }
 
+        // If there are no errors, insert the comment into the database
         if (($ratingError == '') && ($commentError == '')) {
             $comment = $_POST["commenttxt"];
             $thestarRating = $_POST["starRateDDl"];
@@ -52,13 +66,16 @@ if (isset($_POST['btnAddComment'])) {
     }
 }
 
-
-
+// Include the common header
 include("./common/head.php");
 ?>
+
+<!-- Link to the dish CSS stylesheet -->
 <link rel="stylesheet" href="common/styles/dish.css">
+
 <section class="">
     <?php
+    // Retrieve dish details from the database
     try {
         $sql = "SELECT * FROM recipes WHERE id = :foodid;";
         $stmt = $conn->prepare($sql);
@@ -66,55 +83,56 @@ include("./common/head.php");
         $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
         foreach ($result as $row) {
             echo '
-    <p class="sh-header-p">' . $row["type"] . '</p>
-    <h2 class="h2-title">' . $row["thai_name"] . '</span></h2>
-    <br>
-    <div class="dish-container">
-        <div class="dish-image">
-            <img src="' . $row["edited_image"] . '" alt="' . $row["thai_name"] . '">
+        <p class="sh-header-p">' . $row["type"] . '</p>
+        <h2 class="h2-title">' . $row["thai_name"] . '</span></h2>
+        <br>
+        <div class="dish-container">
+            <div class="dish-image">
+                <img src="' . $row["edited_image"] . '" alt="' . $row["thai_name"] . '">
+            </div>
+            <!-- Display dish details -->
+            <div class="dish-box-left">
+                <h3 class="h3-title">
+                    Description
+                </h3>
+                <p class="body">' . $row["description"] . '</p>
+            </div>
+            <div class="dish-box-right">
+                <h3 class="h3-title">
+                    Ingredients
+                </h3>
+                <ul>
+                    <li class="body">' . $row["ingredients"] . '</li>
+                </ul>
+            </div>
+            <div class="dish-box-left">
+                <h3 class="h3-title">
+                    Cooking Time
+                </h3>
+                <p>' . $row["cooking_time_hr"] . ' Hour(s)</p>
+            </div>
+            <div class="dish-box-left">
+                <h3 class="h3-title">
+                    Serves
+                </h3>
+                <p>' . $row["max_serve"] . ' people</p>
+            </div>
         </div>
-        <div class="dish-box-left">
+        <div class="dish-step-box">
             <h3 class="h3-title">
-                Description
-            </h3>
-            <p class="body">' . $row["description"] . '</p>
-        </div>
-        <div class="dish-box-right">
-            <h3 class="h3-title">
-                Ingredients
+                Step
             </h3>
             <ul>
-                <li class="body">' . $row["ingredients"] . '</li>
+                <li class="body">' . $row["step"] . '</li>
             </ul>
         </div>
-        <div class="dish-box-left">
-            <h3 class="h3-title">
-                Cooking Time
-            </h3>
-            <p>' . $row["cooking_time_hr"] . ' Hour(s)</p>
-        </div>
-        <div class="dish-box-left">
-            <h3 class="h3-title">
-                Serves
-            </h3>
-            <p>' . $row["max_serve"] . ' people</p>
-        </div>
-    </div>
-    <div class="dish-step-box">
-        <h3 class="h3-title">
-            Step
-        </h3>
-        <ul>
-            <li class="body">' . $row["step"] . '</li>
-        </ul>
-    </div>
 
-    <div class="dish-note">
-        <h3 class="h3-title">
-            Additional notes :
-        </h3>
-        <p class="body">' . $row["additional_notes"] . '</p>
-    </div>
+        <div class="dish-note">
+            <h3 class="h3-title">
+                Additional notes :
+            </h3>
+            <p class="body">' . $row["additional_notes"] . '</p>
+        </div>
       ';
         }
     } catch (PDOException $e) {
@@ -124,7 +142,7 @@ include("./common/head.php");
     ?>
 </section>
 
-<!-- comment -->
+<!-- Comment section -->
 <section style="background-image: url(common/images/body-bg1.png);">
     <div class="">
         <p class="sh-header-p">Leave A Reply</p>
@@ -134,8 +152,9 @@ include("./common/head.php");
     <div class="review-box flex-row">
         <div class="sum-review ">
             <?php
+            // Retrieve comments and calculate the overall rating
             try {
-                $sql = "SELECT *,COUNT(comment_id) AS num_comment, CAST(AVG(dish_rating)AS DECIMAL(5, 1)) AS overall_rate, ROUND(AVG(dish_rating), 0) AS round_rate FROM comment WHERE dish_id = :foodid;";
+                $sql = "SELECT *,COUNT(comment_id) AS num_comment, CAST(AVG(dish_rating) AS DECIMAL(5, 1)) AS overall_rate, ROUND(AVG(dish_rating), 0) AS round_rate FROM comment WHERE dish_id = :foodid;";
                 $stmt = $conn->prepare($sql);
                 $stmt->execute([':foodid' => $foodId]);
                 $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -155,16 +174,11 @@ include("./common/head.php");
             }
             ?>
         </div>
+        <!-- Comment form -->
         <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" name="myform" class="formBox">
-            <label for="comment"><b>Comment : </b></label><span class="text-error">*<?php echo $commentError; ?></span><br>
+            <label for="comment"><b>Comment : </b></label>
+            <span class="text-error">*<?php echo $commentError; ?></span><br>
             <textarea class="form-control" name="commenttxt" id="comment" value="<?php echo $comment; ?>"></textarea>
-            <!-- <div class="dish-rating">
-                <img src="common/images/star-empty.svg" alt="star-image">
-                <img src="common/images/star-empty.svg" alt="star-image">
-                <img src="common/images/star-empty.svg" alt="star-image">
-                <img src="common/images/star-empty.svg" alt="star-image">
-                <img src="common/images/star-empty.svg" alt="star-image">
-            </div> -->
             <label for="star-rate">Choose rating:</label>
             <select name="starRateDDl" id="star-rate">
                 <option value=0> Select ... </option>
@@ -181,9 +195,10 @@ include("./common/head.php");
     </div>
 </section>
 
-<!-- all comment section -->
+<!-- Display all comments -->
 <section class="all-comment-section">
     <?php
+    // Retrieve all comments for the dish
     try {
         $sql = "SELECT * FROM `comment` INNER JOIN users ON comment.author_id = users.usersId WHERE dish_id = :foodid;";
         $stmt = $conn->prepare($sql);
@@ -208,4 +223,6 @@ include("./common/head.php");
     ?>
 </section>
 <?php
+// Include the common footer
 include('./common/footer.php');
+?>
