3497d8f966ce022e6434d7e64f5ba028ba65908e
Natsasion Srithiowat <100752017+Natsasi-on@users.noreply.github.com>
2023-10-02 14:13:09 -0400

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,18 +1,18 @@
 <?php
 session_start();
 error_reporting(E_ALL & ~E_NOTICE);
+
+// Check if the user is not logged in (userNameDB is not set in the session)
 if (!isset($_SESSION["userNameDB"])) {
+    // Redirect the user to the login page
     header("Location: login.php");
-    exit();
+    exit(); // Terminate script execution
 }
-include("./common/header.php");
-?>
 
+// Destroy the current session, effectively logging the user out
+session_destroy();
 
-<?php session_destroy();
+// Redirect the user to the index page after logging out
 header("Location: index.php");
 exit();
 ?>
-
-<?php
-include('./common/footer.php');
