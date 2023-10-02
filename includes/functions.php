eae5c303ece04bcda147709db5793155b5c63ca0
Natsasion Srithiowat <100752017+Natsasi-on@users.noreply.github.com>
2023-10-02 14:22:12 -0400

    Update functions.php

--- includes/functions.php
+++ includes/functions.php
@@ -1,8 +1,11 @@
 <?php
+// Include the necessary configuration file
 include 'includes/ini.php';
 
+// Function to display star ratings based on the starRate parameter
 function getStarRating($starRate)
 {
+    // Switch statement to determine the star rating and display corresponding stars
     switch ($starRate) {
         case 0:
             for ($i = 0; $i < 5; $i++) {
@@ -46,13 +49,14 @@ function getStarRating($starRate)
     return null;
 }
 
-// fuction for registration new user/////////////////////////////////////////////
+// Function for trimming input data
 function test_input($myInput)
 {
     $myInput = trim($myInput);
     return $myInput;
 }
 
+// Function to validate the name
 function ValidateName($name)
 {
     $nameRegex = "/^([a-zA-Z]{2,}\s[a-zA-Z]{1,}'?-?[a-zA-Z]{2,}\s?([a-zA-Z]{1,})?)/";
@@ -64,6 +68,8 @@ function ValidateName($name)
         return $nameError;
     }
 }
+
+// Function to validate the password
 function ValidatePassword($password)
 {
     $passwordRegex = "/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{6,}$/";
@@ -71,13 +77,12 @@ function ValidatePassword($password)
         $passwordError = '';
         return $passwordError;
     } else {
-        $passwordError = " Password should contain at least 6 characters long, one upper case, one lowercase and one digit!";
+        $passwordError = " Password should contain at least 6 characters long, one upper case, one lowercase, and one digit!";
         return $passwordError;
     }
-    // $passwordError = '';
-    // return $passwordError;
 }
 
+// Function to validate the confirmation of the password
 function ValidateConfirmPassword($confirmPassword)
 {
     if ($_POST["password"] != $_POST["confirmPassword"]) {
@@ -88,6 +93,8 @@ function ValidateConfirmPassword($confirmPassword)
         return $confirmPasswordError;
     }
 }
+
+// Function to validate the email address
 function ValidateEmail($email)
 {
     $EmailAddressRegex = "/\b[a-zA-Z0-9._%+-]+@(([a-zA-Z0-9-]+)\.)+[a-zA-Z]{2,4}\b/";
@@ -99,23 +106,49 @@ function ValidateEmail($email)
         return $emailError;
     }
 }
-///////////////////////////////////////////////////////////////////
+
+// Function to create a new user
 function createUser($name, $password, $postalCode, $PhoneNum, $email)
 {
+    // Include the database connection
     include 'includes/ini.php';
+    
+    // SQL query to insert user data into the database
     $sql = "INSERT INTO users (usersName, usersPwd, usersPost, usersPhone, usersEmail) VALUES (:uName,:uPwd,:uPost,:uPhone,:uEmail);";
+    
+    // Prepare and execute the SQL statement
     $stmt = $conn->prepare($sql);
+    
+    // Hash the password for security
     $hashedPwd = password_hash($password, PASSWORD_DEFAULT);
-    $stmt->execute([':uName' => $name, ':uPwd' => $hashedPwd, ':uPost' => $postalCode, ':uPhone' => $PhoneNum, ':uEmail' => $email]);
+    
+    // Execute the query with provided values
+    $stmt->execute([
+        ':uName' => $name,
+        ':uPwd' => $hashedPwd,
+        ':uPost' => $postalCode,
+        ':uPhone' => $PhoneNum,
+        ':uEmail' => $email
+    ]);
 }
-///////////////////////////////////////////////
+
+// Function to check if an email already exists in the database
 function emailExist($conn, $email)
 {
+    // Include the database connection
     include 'includes/ini.php';
+    
+    // SQL query to select a user by email
     $sql = "SELECT * FROM users WHERE usersEmail=:email;";
+    
+    // Prepare and execute the SQL statement
     $stmt = $conn->prepare($sql);
     $stmt->execute([':email' => $email]);
+    
+    // Fetch the results
     $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    
+    // Check if the result is empty (email doesn't exist)
     if (empty($result)) {
         $myresults = false;
         return $myresults;
@@ -123,14 +156,24 @@ function emailExist($conn, $email)
         return $result;
     }
 }
+
+// Function to check if a user with the same email already exists
 function userExist($conn, $email)
 {
+    // Include the database connection
     include 'includes/ini.php';
+    
+    // SQL query to select a user by email
     $sql = "SELECT * FROM users WHERE usersEmail=:email;";
+    
+    // Prepare and execute the SQL statement
     $stmt = $conn->prepare($sql);
     $stmt->execute([':email' => $email]);
-    //retrieve only one row foreach doesn't require
+    
+    // Retrieve only one row (there should be only one user with the same email)
     $result = $stmt->fetch(PDO::FETCH_ASSOC);
+    
+    // Check if the result is empty (user doesn't exist)
     if (empty($result)) {
         $myresults = false;
         return $myresults;
